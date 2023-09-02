09894c27b4663289153f660d20669e59f89b8372
Shin-Thant <shinthant10684@gmail.com>
2023-09-02 22:59:21 +0630

    make tweet body optional

--- src/schema/tweetSchema.ts
+++ src/schema/tweetSchema.ts
@@ -1,25 +1,20 @@
 import Joi from "joi";
 
-export interface CreateTweetInput {
-	body: string;
-}
+export type CreateTweetInput = {
+	body?: string;
+};
 export const createTweetSchema = Joi.object({
 	body: Joi.object<CreateTweetInput>({
-		body: Joi.string().trim().required().messages({
+		body: Joi.string().trim().optional().messages({
 			"string.base": "Tweet body must be string!",
-			"any.required": "Tweet body is required!",
 		}),
 	}),
 	query: Joi.object({}),
 	params: Joi.object({}),
 });
 
-export type ShareTweetInput = Partial<CreateTweetInput>;
-export const shareTweetSchema = createTweetSchema.keys({
-	body: Joi.object<ShareTweetInput>({
-		body: Joi.string().trim(),
-	}),
-});
+export type ShareTweetInput = CreateTweetInput;
+export const shareTweetSchema = createTweetSchema;
 
 export type EditTweetInput = {
 	body: {
