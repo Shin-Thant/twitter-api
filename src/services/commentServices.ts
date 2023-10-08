932149a927a28dbd97b046051b3bdddb12892431
Shin-Thant <shinthant10684@gmail.com>
2023-10-09 00:27:48 +0630

    add create comment service

--- src/services/commentServices.ts
+++ src/services/commentServices.ts
@@ -2,6 +2,15 @@ import Comment from "../models/Comment";
 import { CommentSchema } from "../models/types/commentTypes";
 import { DeleteMany } from "./types";
 
+interface CreateCommentData {
+	body: string;
+	owner: string;
+	tweet: string;
+}
+export async function createComment(data: CreateCommentData) {
+	return await Comment.create(data);
+}
+
 export async function deleteComments(args: DeleteMany<CommentSchema>) {
-	return Comment.deleteMany(args.filter, args.options);
+	return await Comment.deleteMany(args.filter, args.options);
 }
