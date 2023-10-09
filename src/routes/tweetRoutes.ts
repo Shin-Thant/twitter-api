182f0bd1247c5fef72b0a6f1d998914c29bde643
Shin-Thant <shinthant10684@gmail.com>
2023-10-10 00:59:14 +0630

    add comment route as nested route

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,25 +1,25 @@
 import { Router } from "express";
-import { getTweetComments } from "../controllers/commentController";
 import {
 	createTweetHandler,
 	deleteTweetHandler,
+	editTweetHandler,
 	getTweetById,
 	getTweets,
 	handleLikes,
 	shareTweet,
-	editTweetHandler,
 } from "../controllers/tweetController";
+import { uploadMany } from "../middlewares/imageUpload";
+import { saveTweetImages } from "../middlewares/saveTweetImages";
+import { tweetBodyOrImage } from "../middlewares/tweetBodyOrImage";
+import validateResource from "../middlewares/validateResource";
 import verifyJWT from "../middlewares/verifyJWT";
 import verifyTweetOwner from "../middlewares/verifyTweetOwner";
-import validateResource from "../middlewares/validateResource";
 import {
 	createTweetSchema,
 	editTweetSchema,
 	getTweetByIdSchema,
 } from "../schema/tweetSchema";
-import { uploadMany } from "../middlewares/imageUpload";
-import { saveTweetImages } from "../middlewares/saveTweetImages";
-import { tweetBodyOrImage } from "../middlewares/tweetBodyOrImage";
+import commentRoutes from "./commentRoutes";
 
 const router = Router();
 
@@ -59,6 +59,10 @@ router.route("/:tweetId/like").patch(verifyJWT, handleLikes);
 
 router.route("/:tweetId/share").post(verifyJWT, shareTweet);
 
-router.route("/:tweetId/comments").get(getTweetComments);
+//router.route("/:tweetId/comments").get(getTweetComments);
+router.use(
+	"/:tweetId/comments",
+	commentRoutes
+);
 
 export default router;
