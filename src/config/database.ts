460fb721a5d375505114497f805503a1adee2b17
Shin-Thant <shinthant10684@gmail.com>
2023-11-24 13:23:38 +0630

    refactor: use local database in other than production environment

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-	await mongoose.connect(process.env.DATABASE_URI);
-	// await mongoose.connect(process.env.LOCAL_DATABASE_URI);
+	const DATABASE_URI: string =
+		process.env.NODE_ENV !== "production"
+			? "mongodb://localhost:27017/Twitter"
+			: process.env.DATABASE_URI;
+
+	await mongoose.connect(DATABASE_URI);
 }
 
 export async function disconnectDB() {
